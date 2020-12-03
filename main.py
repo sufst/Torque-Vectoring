d633d0477777273d5a4c6f9c4bcbe6d697a4d99a
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2020-12-03 18:50:22 +0000

    Added functions to be filled in later that are there to show structure of algorithm and help clarify steps to implement algorith, more will be added later.

---
+++ main.py
@@ -0,0 +1,51 @@
+"""Just writing this so you can fill in the functions, hopefully should add clarity about
+the structure of the algorithm"""
+
+import numpy as np
+
+"""
+No C like struct available in Python, so a class will do for now.
+"""
+
+
+class VehicleVelocity:
+    """
+    Vector of vehicle velocity
+    """
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.mag = np.sqrt(x ** 2 + y ** 2)
+
+
+class InertialForces:
+    """
+    Vector of acceleration components felt by car
+    """
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.mag = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+
+
+velocity_mps = VehicleVelocity(5, 0.2)
+
+
+def turning_radius_meters(v_mps, steering_angle_rad):
+    """
+    :param v_mps: Vehicle velocity. x and y components can be accesses with v_mps.x and
+    v_mps.y respecitvely.
+    :param steering_angle_rad: Steering angle in radians
+    :return: The turn radius of a perfect circle if v_mps and steering_angle_rad are constants
+    """
+    return 0
+
+def ideal_inertial_forces(v_mps, turn_r_m):
+    """
+    :param v_mps: Vehicle velocity. x and y components can be accesses with v_mps.x and
+    v_mps.y respecitvely.
+    :param turn_r_m: Instantaneous effective turn radius of perfect circle
+    :return: Vector of ideal instantaneous acceleration the CG should feel
+    """
+    return 0
